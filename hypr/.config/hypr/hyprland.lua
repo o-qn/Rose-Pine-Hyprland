97e77437808b6837a8a38d0ec6682487820e2f89
o-qn <onajeh22e@gmail.com>
2026-09-12 17:27:38 +0300

    Add Hyprlock and idle power management

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -54,6 +54,7 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("mako")
 	hl.exec_cmd("waybar")
 	hl.exec_cmd("wl-paste --watch cliphist store")
+	hl.exec_cmd("hypridle")
 end)
 
 -------------------------------
